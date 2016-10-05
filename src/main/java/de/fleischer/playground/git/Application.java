49fbfe30144f7fd5bbb4a16a3cf3d0d2dcef0eb2
Sean Fleischer <sean.fleischer@comsysto.com>
2016-10-05 08:05:56 +0200

    Rebase test

--- src/main/java/de/fleischer/playground/git/Application.java
+++ src/main/java/de/fleischer/playground/git/Application.java
@@ -26,5 +26,7 @@ public class Application {
         for (String beanName : beanNames) {
             System.out.println(beanName);
         }
+        
+        System.out.println("Test");
     }
 }
